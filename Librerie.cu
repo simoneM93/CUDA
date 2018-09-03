94a2bcb9bd3db786e164fb78036a482f0a7802ba
Simone <simone.marano@skylabs.it>
2018-09-03 17:13:58 +0200

    Created Librerie.cu

---
+++ Librerie.cu
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <malloc.h>
+#include <cuda_runtime_api.h>
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "cuda_fp16.h"
+
+//Metodi Implementati Per L'Esecuzione Del Metodo Di Jacobi
+#include "DiagonalyDominantMatrix.cu" //Serve a controllare se la matrice è Strettamente Diagonalmente Dominante
+#include "InitMatrix.cu" //Inizializza la matrice
+#include "InitVector.cu" //Inizializza il vettore
+#include "SumMatrix.cu" //Esegue la somma tra matrici
+#include "TransposedMatrix.cu" //Calcola la trasposta di una matrice
+#include "MatrixDivision.cu" //Divide la matrice in Matrice Diagonale e Matrice Triangolare Superiore e Inferiore
+#include "MoltiplicationMatrixVector.cu" //Esegue la moltiplicazione tra matrice e vettore
+#include "SumVectorVector.cu" //Esegue la somma tra due vettori
+#include "NormaDue.cu" //Calcola la Norma Due di un vettore
+#include "Check_Cuda.cu" //Errori
