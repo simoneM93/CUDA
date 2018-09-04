2797132db0eb3323437f20b58468d438e9936181
Simone <simone.marano@skylabs.it>
2018-09-04 10:09:45 +0200

    DiffVectorVector

---
+++ DiffVectorVector.cu
@@ -0,0 +1,6 @@
+__global__ void diffVectorVector(int dim, int* vector1, int* vector2, int* result)
+{
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+	result[index] = vector1[index] - vector2[index];
+}
